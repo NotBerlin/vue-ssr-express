11fa664a19e0019cf28f3f158a44460d7d6abfb8
chenluolin <917988385@qq.com>
2021-07-31 13:11:43 +0800

    feat: first

---
+++ vue-ssr-express/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">首页</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped>
+#app {
+  margin: 0;
+  padding: 0;
+  font-size: 14px;
+}
+</style>
